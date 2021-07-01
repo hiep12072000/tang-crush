6f0698153221fa082c75a096f3338a8f27632314
hiep12072000 <65528702+hiep12072000@users.noreply.github.com>
2021-07-01 14:11:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Hi cậu!',
+    introTitle: 'Hi Yến!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao cậu cứ đi nhầm đường
     Lạc vào tim tớ lẻ loi
@@ -7,13 +7,13 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích cậu <33',
-    btnNo: 'Không, chúng ta chỉ là bạn :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại chọn tớ <3',
-    btnReply: 'Gửi cho cậu <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h tớ qua đón nhé.',
+    btnYes: 'Đi chơi với tớ <33',
+    btnNo: 'Thồi, tớ bận :3',
+    question:'Cậu rảnh hôm nào nào <3',
+    btnReply: 'Set lịch thôi, nhớ quá <3',
+    reply: '<33333333',
+    mess: 'Tớ biết mà 🥰hihi 😘😘',
+    messDesc: 'Tối đó 7h tớ qua đón nhé.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
